9f012e008e342b5951104194fe3a424d9ca63458
Ashley Echols <ashleyne92@gmail.com>
2019-02-06 15:13:32 -0600

    Adds beginning of Navbar component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import MovieNav from '../MovieNav/MovieNav';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <h1>Hello World!</h1>
+        <MovieNav />
       </div>
     );
   }
---
+++ src/components/MovieNav/MovieNav.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {
+  Container,
+  FormGroup,
+  Input,
+  Label,
+  Nav,
+  NavItem,
+  NavLink,
+  Navbar,
+  NavbarBrand,
+} from 'reactstrap';
+import './MovieNav.scss';
+
+class MovieNav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Navbar className="navbar">
+        <Container>
+          <NavbarBrand href="/" className="navbar__logo">
+            Movie Directory
+          </NavbarBrand>
+
+          <Nav>
+            <NavLink active href="#" className="navbar__link">
+              Browse
+            </NavLink>
+            <NavLink href="#" className="navbar__link">
+              Credits
+            </NavLink>
+
+            <FormGroup className="navbar__form">
+              <Input type="search" name="search" />
+            </FormGroup>
+          </Nav>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default MovieNav;
